978481bac835358b013e8a4e961d8e9b05f68170
miskhodzh <miskhodzhev.osman@yandex.ru>
2024-01-25 12:23:47 +0300

    Настроил вход

--- project_management/accounts/urls.py
+++ project_management/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- project_management/accounts/views.py
+++ project_management/accounts/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+@login_required
 def index(request):
     template = 'accounts/index.html'
+    context = {'user': User}
     return render(request, template)
 
-# def login(request):
-#     template = 'accounts/login.html'
-#     if request.method == 'POST':
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = authenticate(username=username, password=password)
-#     else:
-#         form = UserLoginForm()
-    
-#     context = {'form': form}
-#     return render(request, template)
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid:
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse('accounts:index'))
+    else:
+        form = AuthenticationForm()
+
+    context = {'form': form}
+    template = 'accounts/login.html'
+    return render(request, template, context)
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('accounts:login'))
